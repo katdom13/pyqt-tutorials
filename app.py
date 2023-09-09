4d7bb81aaa33a2ff35a4fbdaa470fbb5d17dffe3
katherine.domingo <katherine.domingo@torchbox.com>
2023-09-09 21:24:50 +0800

    PyQt6 Widgets Using QPushButton, QCheckBox, QComboBox, QLabel and QSlider widgets

--- app.py
+++ app.py
@@ -1,55 +1,49 @@
-# https://www.pythonguis.com/tutorials/pyqt6-signals-slots-events/
-
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+# https://www.pythonguis.com/tutorials/pyqt6-widgets/
 
 import sys
-from random import choice
 
-window_titles = [
-    'My App',
-    'My App',
-    'Still My App',
-    'Still My App',
-    'What on earth',
-    'What on earth',
-    'This is surprising',
-    'This is surprising',
-    'Something went wrong'
-]
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QSlider,
+)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.n_times_clicked = 0
-
         self.setWindowTitle("My App")
 
-        self.button = QPushButton("Press Me!")
-        self.button.clicked.connect(self.the_button_was_clicked)
+        widget = QSlider()
 
-        self.windowTitleChanged.connect(self.the_window_title_changed)
+        widget.setMinimum(-10)
+        widget.setMaximum(3)
+        # Or: widget.setRange(-10,3)
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(self.button)
+        widget.setSingleStep(3)
 
-    def the_button_was_clicked(self):
-        print("Clicked.")
-        new_window_title = choice(window_titles)
-        print("Setting title:  %s" % new_window_title)
-        self.setWindowTitle(new_window_title)
+        widget.valueChanged.connect(self.value_changed)
+        widget.sliderMoved.connect(self.slider_position)
+        widget.sliderPressed.connect(self.slider_pressed)
+        widget.sliderReleased.connect(self.slider_released)
 
-    def the_window_title_changed(self, window_title):
-        print("Window title changed: %s" % window_title)
+        self.setCentralWidget(widget)
 
-        if window_title == 'Something went wrong':
-            self.button.setDisabled(True)
+    def value_changed(self, i):
+        print(i)
+
+    def slider_position(self, p):
+        print("position", p)
+
+    def slider_pressed(self):
+        print("Pressed!")
+
+    def slider_released(self):
+        print("Released")
 
 
 app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
+w = MainWindow()
+w.show()
 app.exec()
