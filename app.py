20ec3b163986f0b52a3a7db184215ba23b12ce14
katherine.domingo <katherine.domingo@torchbox.com>
2023-09-13 21:18:27 +0800

    Creating additional windows Opening new windows for your application

--- app.py
+++ app.py
@@ -1,52 +1,64 @@
-# https://www.pythonguis.com/tutorials/pyqt6-dialogs/
+# https://www.pythonguis.com/tutorials/pyqt6-creating-multiple-windows/
 
 import sys
+from random import randint
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QDialogButtonBox, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent,
+    it will appear as a free-floating window.
+    """
+
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Another Window % d" % randint(0, 100))
+        layout.addWidget(self.label)
+        self.setLayout(layout)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.window1 = AnotherWindow()
+        self.window2 = AnotherWindow()
 
-        self.setWindowTitle("My App")
+        l = QVBoxLayout()
+        button1 = QPushButton("Push for Window 1")
+        button1.clicked.connect(
+            lambda checked: self.toggle_window(self.window1)
+        )
+        l.addWidget(button1)
 
-        button = QPushButton("Press me for a dialog!")
-        button.clicked.connect(self.button_clicked)
-        self.setCentralWidget(button)
+        button2 = QPushButton("Push for Window 2")
+        button2.clicked.connect(
+            lambda checked: self.toggle_window(self.window2)
+        )
+        l.addWidget(button2)
 
-    def button_clicked(self, s):
-        print("click", s)
+        w = QWidget()
+        w.setLayout(l)
+        self.setCentralWidget(w)
 
-        dlg = CustomDialog(self)
-        if dlg.exec():
-            print("Success!")
+    def toggle_window(self, window):
+        if window.isVisible():
+            window.hide()
+
         else:
-            print("Cancel!")
-
-
-class CustomDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        self.setWindowTitle("HELLO!")
-
-        QBtn = QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
-
-        self.buttonBox = QDialogButtonBox(QBtn)
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
-
-        self.layout = QVBoxLayout()
-        message = QLabel("Something happened, is that OK?")
-        self.layout.addWidget(message)
-        self.layout.addWidget(self.buttonBox)
-        self.setLayout(self.layout)
+            window.show()
 
 
 app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
+w = MainWindow()
+w.show()
 app.exec()
