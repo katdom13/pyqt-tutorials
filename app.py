dd5cbcf116583b439bae02c48ebd394d0a4a8338
katherine.domingo <katherine.domingo@torchbox.com>
2023-09-16 00:34:54 +0800

    First steps with Qt Designer Use Qt Designer's drag and drop interface to design your PyQt6 GUI

--- app.py
+++ app.py
@@ -1,64 +1,26 @@
-# https://www.pythonguis.com/tutorials/pyqt6-creating-multiple-windows/
+# https://www.pythonguis.com/tutorials/pyqt6-first-steps-qt-designer/
+
+# First steps with Qt Designer
+# Use Qt Designer's drag and drop interface to design your PyQt6 GUI
 
 import sys
-from random import randint
+from PyQt6 import QtWidgets, uic
 
-from PyQt6.QtWidgets import (
-    QApplication,
-    QLabel,
-    QMainWindow,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-)
+from MainWindow import Ui_MainWindow
 
 
-class AnotherWindow(QWidget):
-    """
-    This "window" is a QWidget. If it has no parent,
-    it will appear as a free-floating window.
-    """
-
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.label = QLabel("Another Window % d" % randint(0, 100))
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.window1 = AnotherWindow()
-        self.window2 = AnotherWindow()
+app = QtWidgets.QApplication(sys.argv)
 
-        l = QVBoxLayout()
-        button1 = QPushButton("Push for Window 1")
-        button1.clicked.connect(
-            lambda checked: self.toggle_window(self.window1)
-        )
-        l.addWidget(button1)
-
-        button2 = QPushButton("Push for Window 2")
-        button2.clicked.connect(
-            lambda checked: self.toggle_window(self.window2)
-        )
-        l.addWidget(button2)
-
-        w = QWidget()
-        w.setLayout(l)
-        self.setCentralWidget(w)
-
-    def toggle_window(self, window):
-        if window.isVisible():
-            window.hide()
-
-        else:
-            window.show()
-
-
-app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
+window = MainWindow()
+window.show()
 app.exec()
+
+
+# Converting your .ui file to Python
+# pyuic6 mainwindow.ui -o MainWindow.py
