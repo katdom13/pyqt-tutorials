77b5c5a0a6489d4a6a580506de1d8fbec052fe42
katherine.domingo <katherine.domingo@torchbox.com>
2023-09-19 21:07:22 +0800

    Displaying tabular data in Qt6 ModelViews Create customized table views with conditional formatting, numpy and pandas data sources.

--- app.py
+++ app.py
@@ -1,103 +1,116 @@
-# https://www.pythonguis.com/tutorials/pyqt6-modelview-architecture/
+# https://www.pythonguis.com/tutorials/pyqt6-qtableview-modelviews-numpy-pandas/
 
-# The ModelView Architecture
-# Qt's MVC-like interface for displaying data in views
+# Displaying tabular data in Qt6 ModelViews
+# Create customized table views with conditional formatting, numpy and pandas data sources.
 
 import sys
-import json
-from PyQt6 import QtCore, QtGui, QtWidgets, uic
+from datetime import datetime
+from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtCore import Qt
+import pandas as pd
+
+COLORS = ['#053061', '#2166ac', '#4393c3', '#92c5de', '#d1e5f0', '#f7f7f7', '#fddbc7', '#f4a582', '#d6604d', '#b2182b', '#67001f']
 
 
-qt_creator_file = "todo.ui"
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qt_creator_file)
-tick = QtGui.QImage('tick.png')
-
-
-class TodoModel(QtCore.QAbstractListModel):
-    def __init__(self, *args, todos=None, **kwargs):
-        super(TodoModel, self).__init__(*args, **kwargs)
-        self.todos = todos or []
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
 
     def data(self, index, role):
         if role == Qt.ItemDataRole.DisplayRole:
-            _, text = self.todos[index.row()]
-            return text
+            # Get the raw value
+            value = self._data.iloc[index.row(), index.column()]
+
+            # Perform per-type checks and render accordingly.
+            if isinstance(value, datetime):
+                # Render time to YYY-MM-DD.
+                return str(value.strftime("%Y-%m-%d"))
+
+            if isinstance(value, float):
+                # Render float to 2 dp
+                return str("%.2f" % value)
+
+            if isinstance(value, str):
+                # Render strings with quotes
+                return str('"%s"' % value)
+
+            # Default (anything not captured above: e.g. int)
+            return str(value)
+
+        if role == Qt.ItemDataRole.TextAlignmentRole:
+            value = self._data.iloc[index.row(), index.column()]
+
+            if isinstance(value, int) or isinstance(value, float):
+                # Align right, vertical middle.
+                return Qt.AlignmentFlag.AlignVCenter + Qt.AlignmentFlag.AlignRight
+
+        if role == Qt.ItemDataRole.ForegroundRole:
+            value = self._data.iloc[index.row(), index.column()]
+
+            if (
+                (isinstance(value, int) or isinstance(value, float))
+                and value < 0
+            ):
+                return QtGui.QColor('red')
+
+        if role == Qt.ItemDataRole.BackgroundRole:
+            value = self._data.iloc[index.row(), index.column()]
+            if (isinstance(value, int) or isinstance(value, float)):
+                value = int(value)  # Convert to integer for indexing.
+
+                # Limit to range -5 ... +5, then convert to 0..10
+                value = max(-5, value) # values < -5 become -5
+                value = min(5, value)  # valaues > +5 become +5
+                value = value + 5     # -5 becomes 0, +5 becomes + 10
+
+                return QtGui.QColor(COLORS[value])
 
         if role == Qt.ItemDataRole.DecorationRole:
-            status, _ = self.todos[index.row()]
-            if status:
-                return tick
+            value = self._data.iloc[index.row(), index.column()]
+            if isinstance(value, datetime):
+                return QtGui.QIcon('calendar.png')
 
     def rowCount(self, index):
-        return len(self.todos)
+        # The length of the outer list.
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role):
+        # section is the index of the column/row.
+        if role == Qt.ItemDataRole.DisplayRole:
+            if orientation == Qt.Orientation.Horizontal:
+                return str(self._data.columns[section])
+
+            if orientation == Qt.Orientation.Vertical:
+                return str(self._data.index[section])
 
 
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        self.setupUi(self)
-        self.model = TodoModel()
-        self.load()
-        self.todoView.setModel(self.model)
-        self.addButton.pressed.connect(self.add)
-        self.deleteButton.pressed.connect(self.delete)
-        self.completeButton.pressed.connect(self.complete)
+        super().__init__()
 
-    def add(self):
-        """
-        Add an item to our todo list, getting the text from the QLineEdit .todoEdit
-        and then clearing it.
-        """
-        text = self.todoEdit.text()
-        if text: # Don't add empty strings.
-            # Access the list via the model.
-            self.model.todos.append((False, text))
-            # Trigger refresh.
-            self.model.layoutChanged.emit()
-            # Empty the input
-            self.todoEdit.setText("")
-            self.save()
+        self.table = QtWidgets.QTableView()
 
-    def delete(self):
-        indexes = self.todoView.selectedIndexes()
-        if indexes:
-            # Indexes is a list of a single item in single-select mode.
-            index = indexes[0]
-            # Remove the item and refresh.
-            del self.model.todos[index.row()]
-            self.model.layoutChanged.emit()
-            # Clear the selection (as it is no longer valid).
-            self.todoView.clearSelection()
-            self.save()
+        data = pd.DataFrame([
+            [True, 9, 2],
+            [1, 0, -1],
+            [3, 5, False],
+            [3, 3, 2],
+            [datetime(2019, 5, 4), 8, 9],
+        ], columns = ['A', 'B', 'C'], index=['Row 1', 'Row 2', 'Row 3', 'Row 4', 'Row 5'])
 
-    def complete(self):
-        indexes = self.todoView.selectedIndexes()
-        if indexes:
-            index = indexes[0]
-            row = index.row()
-            status, text = self.model.todos[row]
-            self.model.todos[row] = (True, text)
-            # .dataChanged takes top-left and bottom right, which are equal
-            # for a single selection.
-            self.model.dataChanged.emit(index, index)
-            # Clear the selection (as it is no longer valid).
-            self.todoView.clearSelection()
-            self.save()
+        self.model = TableModel(data)
+        self.table.setModel(self.model)
 
-    def load(self):
-        try:
-            with open('data.db', 'r') as f:
-                self.model.todos = json.load(f)
-        except Exception:
-            pass
-
-    def save(self):
-        with open('data.db', 'w') as f:
-            data = json.dump(self.model.todos, f)
+        self.setCentralWidget(self.table)
 
 
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
+app=QtWidgets.QApplication(sys.argv)
+window=MainWindow()
 window.show()
 app.exec()
